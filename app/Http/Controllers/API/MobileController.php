52f076c04bfa681db45c99559525db6a833a7118
root <daniel.ntaye@sahanalytics.com>
2024-12-21 14:53:08 +0000

    upgrading mobile auth

--- app/Http/Controllers/API/MobileController.php
+++ app/Http/Controllers/API/MobileController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use F9Web\ApiResponseHelpers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MobileController extends Controller
 {
-    //
+
+    use ApiResponseHelpers;
+
+
+    public function store_acte(Request $request)
+    {
+        Validator::validate($request->input(), [
+            'type_acte'=>'required'
+        ]);
+        if(!$request->filled('type_acte')){
+            $this->respondError("Le champ type_acte doit être fournis");
+        }
+    }
 }
