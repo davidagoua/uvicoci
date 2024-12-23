095fff54a80aa726b3f66d3ade526ab4047c5d98
root <daniel.ntaye@sahanalytics.com>
2024-12-23 22:22:48 +0000

    modified:   resources/views/filament/pages/acte-deces-details.blade.php

--- resources/views/filament/pages/acte-deces-details.blade.php
+++ resources/views/filament/pages/acte-deces-details.blade.php
@@ -1,5 +1,7 @@
 <x-filament-panels::page>
     <script defer src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <div class="bg-white rounded p-3 shadow">
     <div class="  p-3 ">
         <fieldset>
@@ -25,19 +27,19 @@
                 </div>
                 <div class="my-5">
                     <h5 class="">
-                            <span class="heroicon-o-arrow-down-tray"></span>
+                            
                             <a href="{{ asset('storage/'.$this->acteDeces->pv_deces) }}" class="p-1 rounded-lg border hover:bg-amber-50 font-semisemibold shadow border-amber-700 text-amber-700 bg-white">
-                                <span class="heroicon-o-arrow-down-tray"></span>
-                               
+                                <span class="fa fa-download"></span>
+                                PV de decés
                             </a>
-                            PV de decés
+                            
                         
                     </h5>
                 </div>
                 <div class="my-5">
                     <h5 class="">
                         <a href="{{ asset('storage/'.$this->acteDeces->piece_identite) }}" class="p-1 rounded-lg border hover:bg-amber-50 font-semisemibold shadow border-amber-700 text-amber-700 bg-white">
-                            <span class="heroicon-o-arrow-down-tray"></span>
+                            <span class="fa fa-download"></span>
                             Pièce d'identité
                         </a>
                     </h5>
