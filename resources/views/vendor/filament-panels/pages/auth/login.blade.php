ab13a9ad0fd0991f9d36edecb1535339ae177d90
root <daniel.ntaye@sahanalytics.com>
2024-12-23 18:50:53 +0000

    publish style

---
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -0,0 +1,33 @@
+<style>
+    body {
+        background-image: url('/images/bg.jpg');
+        background-size: cover;
+
+    }
+    .fi-logo{
+        width: auto;
+        height: 150px !important;
+    }
+</style>
+<x-filament-panels::page.simple>
+    @if (filament()->hasRegistration())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+
+            {{ $this->registerAction }}
+        </x-slot>
+    @endif
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.before') }}
+
+    <x-filament-panels::form wire:submit="authenticate">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+    </x-filament-panels::form>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.after') }}
+</x-filament-panels::page.simple>
